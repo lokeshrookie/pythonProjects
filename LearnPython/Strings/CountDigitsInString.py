6c5a1e7e9cec06452db5622999774aa9660de25d
Lokesh <rocklinglokesh@gmail.com>
2021-10-02 18:22:33 +0530

    CountDigitsInString.py add

--- LearnPython/Strings/CountDigitsInString.py
+++ LearnPython/Strings/CountDigitsInString.py
@@ -2,7 +2,7 @@ text = input()
 count = 0
 for i in text:
     if i.isdigit():
-        count +=1
+        count += 1
 print(count)
 print(text.count('a')) # this line counts the no of 'a' in text.
 
