751368d50027991890e5d3039545f969f955ce1f
Lokesh <rocklinglokesh@gmail.com>
2021-10-20 22:08:35 +0530

    Tuples added comments

--- LearnPython/Tuples/__init__.py
+++ LearnPython/Tuples/__init__.py
@@ -1,5 +1,5 @@
 # Tuples are immutable where lists are immutable
-tup = (1,2,3,4,5)
-tup.count(3)
+tup = (1, 2, 3, 4, 5)
+tup.count(3)  # Counts the no of duplicates of a given element
 print(tup.count(5))
-print(tup.index(5))
+print(tup.index(5))     # Prints the index of a given element
