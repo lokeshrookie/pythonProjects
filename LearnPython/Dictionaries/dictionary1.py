c102e15f89ff6b5379a3cc58a0ecb99008548b45
Lokesh <rocklinglokesh@gmail.com>
2021-10-20 22:14:50 +0530

    initial commit

--- LearnPython/Dictionaries/dictionary1.py
+++ LearnPython/Dictionaries/dictionary1.py
@@ -4,6 +4,5 @@ they store key value pairs. We can access value of any key individually
 """
 
 students = {"S1": "IBM", "S2": ["Google", "Microsoft", "Amazon"]}
-
 print(students["S1"])
 print(students["S2"][0])  # Prints only value at 0th index.
