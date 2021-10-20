5d4a15edbf143d73a15a38c26a4f4d0a66a38fda
Lokesh <rocklinglokesh@gmail.com>
2021-10-20 22:14:05 +0530

    initial commit

--- LearnPython/Dictionaries/dictionary1.py
+++ LearnPython/Dictionaries/dictionary1.py
@@ -1,3 +1,7 @@
+"""
+Dictonaries are like hash tables in java.
+they store key value pairs. We can access value of any key individually
+"""
 students = {"S1": "IBM", "S2":["Google", "Microsoft", "Amazon"]}
 
 print(students["S1"])
