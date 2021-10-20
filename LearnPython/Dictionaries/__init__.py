6a28f8e997879521261fc8941cbabb278eccd1de
Lokesh <rocklinglokesh@gmail.com>
2021-10-20 21:13:10 +0530

    Dictionary Created

---
+++ LearnPython/Dictionaries/__init__.py
@@ -0,0 +1,7 @@
+dict = {"name": "Lokesh", "Surname": "Darapureddy", "Rollnumber": 29, "Age": 21}
+print(dict["name"])
+print(dict["Age"])
+print(dict.keys())
+print(dict.values())
+dict.pop("Age") # Remove something from dictionary.
+print(dict)
--- LearnPython/Strings/strings.py
+++ LearnPython/Strings/strings.py
@@ -5,5 +5,3 @@ for i in range(len(str)):
         print(str[i])
     else:
         continue
-
-
