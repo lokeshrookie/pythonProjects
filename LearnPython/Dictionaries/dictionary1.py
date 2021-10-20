eaf2a9d76ba794c0d318b15a8826ff522a2de086
Lokesh <rocklinglokesh@gmail.com>
2021-10-20 22:14:37 +0530

    initial commit

--- LearnPython/Dictionaries/dictionary1.py
+++ LearnPython/Dictionaries/dictionary1.py
@@ -2,7 +2,8 @@
 Dictonaries are like hash tables in java.
 they store key value pairs. We can access value of any key individually
 """
-students = {"S1": "IBM", "S2":["Google", "Microsoft", "Amazon"]}
+
+students = {"S1": "IBM", "S2": ["Google", "Microsoft", "Amazon"]}
 
 print(students["S1"])
-print(students["S2"][0])        # Prints only value at 0th index.
+print(students["S2"][0])  # Prints only value at 0th index.
