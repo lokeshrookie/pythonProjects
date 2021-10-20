a50910a95ad48b42e873caec0ee51b8559e23c94
Lokesh <rocklinglokesh@gmail.com>
2021-10-20 21:18:45 +0530

    Dictionary Created

---
+++ LearnPython/LIst/__init__.py
@@ -0,0 +1,7 @@
+lis = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(lis[0])
+print(lis[0:2])  # Starting from 0, it will print two elements
+print(lis[0:])  # Starting from 0, it will print all elements
+print(lis[:])  # Prints all the elements
+print(lis[:5])  # Prints all the elements from start upto (5-1)
+
