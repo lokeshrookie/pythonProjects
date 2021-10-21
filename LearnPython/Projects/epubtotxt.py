33475b29873e0f44c779ac6b0bda94e2cde25ddd
Lokesh <rocklinglokesh@gmail.com>
2021-10-21 14:43:18 +0530

    initial commit

---
+++ LearnPython/Projects/epubtotxt.py
@@ -0,0 +1,10 @@
+from epub2txt import epub2txt
+# from a url to epub
+# url = "https://github.com/ffreemt/tmx2epub/raw/master/tests/1.tmx.epub"
+url = "note8pro\coding and dsa\dsajava7days.epub"
+res = epub2txt(url)
+
+
+# # from a local epub file
+# filepath = r"tests\test.epub"
+# res = epub2txt(filepath)s
