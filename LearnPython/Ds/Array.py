d6faf5a7c5e245e202e607e39b1e9a1a36544595
Lokesh <rocklinglokesh@gmail.com>
2021-10-31 18:11:06 +0530

    python Array.py add

---
+++ LearnPython/Ds/Array.py
@@ -0,0 +1,19 @@
+
+import array as arr
+
+# definition
+
+a = arr.array('d', [1, 2, 3, 4, 5, 6, 7])
+
+# adding
+
+a.append(88)
+
+a.extend([22, 44,])
+
+print(a)
+
+a.reverse()
+
+
+print(a)
---
+++ LearnPython/Ds/Queue.py
@@ -0,0 +1,11 @@
+
+"""
+1. FIFO principle
+2. Oopetation on two ends (front and back)
+3. Insertion = Enque & Deletion = Deque
+"""
+
+
+
+
+
