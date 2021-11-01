64d1bb4d1d527ea333ff677dd7cc62f6ad10001b
Lokesh <rocklinglokesh@gmail.com>
2021-11-01 14:32:37 +0530

    BInarySearchRecursion.py Done

--- LearnPython/Algorithms/Recursion/BInarySearchRecursion.py
+++ LearnPython/Algorithms/Recursion/BInarySearchRecursion.py
@@ -1,20 +1,22 @@
-arr = [-111, -93, -11,  0, 1, 2, 3, 4, 5, 6, 7]
-
-target = -11
 
 
-def binary(lis, element):
-    start = 0
-    end = len(lis) - 1
-    while start <= end:
-        mid = (start + end) // 2  # use // operator
-        if element == lis[mid]:
-            return mid
-        elif element < lis[mid]:
-            end = mid - 1
-        else:
-            start = start + 1
+
+array = [-11, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+key = -11
+
+def binary_search(arr, target, start, end):
+
+    if start > end:
+        return -1
+    mid = (start+end)//2  # use floor division operator
+    if target == arr[mid]:
+        return mid
+    elif target < arr[mid]:
+        return binary_search(arr,target, start, mid-1)
+    elif target > arr[mid]:
+        return binary_search(arr, target, mid+1, end)
     return -1
 
 
-print(binary(arr, target))
+print(binary_search(array, key, 0, len(array) - 1))
