5b550270b41078f40ac3bc9d5725c3183d8885be
Lokesh <rocklinglokesh@gmail.com>
2021-11-09 23:10:05 +0530

    initial commit

--- LearnPython/Algorithms/Recursion/printNto1.py
+++ LearnPython/Algorithms/Recursion/printNto1.py
@@ -2,9 +2,10 @@ n = int(input())
 
 
 def print_numbers(num):
-        print(num)
-        if num == 1:
-            return
-        print_numbers(num-1)
+    print(num)
+    if num == 1:
+        return
+    print_numbers(num - 1)
+
 
 print_numbers(n)
